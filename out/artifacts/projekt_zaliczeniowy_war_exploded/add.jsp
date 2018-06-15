a2e3226815ae65b497e4f36ba59ca54e55c98191
Aleksander Dytko <olek.dytko@gmail.com>
2018-06-15 18:31:22 +0200

    Klasy JSP oraz servlet

---
+++ out/artifacts/projekt_zaliczeniowy_war_exploded/add.jsp
@@ -0,0 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Win8
+  Date: 2018-06-15
+  Time: 16:38
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Add Movie</title>
+
+</head>
+<body>
+<div style="text-align: center;"><form method="post" action="addMovie">
+    <input type="text" name="title" placeholder="Tytuł"><br>
+    <input type="text" name="director" placeholder="Reżyser"><br>
+    <input type="text" name="url" placeholder="URL"><br>
+    <input type="text" name="year" placeholder="Rok produkcji"><br>
+    <input type="submit" value="Dodaj">
+</form></div>
+</body>
+</html>
---
+++ src/Movies.java
@@ -0,0 +1,35 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(name = "/addMovie")
+public class Movies extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.setCharacterEncoding("UTF-8");
+        String title = request.getParameter("title");
+        String director = request.getParameter("director");
+        String URL = request.getParameter("url");
+        String year = request.getParameter("year");
+
+        if (title.isEmpty()&&director.isEmpty()&&URL.isEmpty()&&year.isEmpty()){
+            request.getRequestDispatcher("index.jsp").forward(request, response);
+        }else {
+            Movie movie=new Movie(title,director,URL,year);
+            MoviesDAO moviesDAO=new MoviesDAO();
+            moviesDAO.addMovie(movie);
+            request.getRequestDispatcher("index.jsp").forward(request, response);
+        }
+
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
