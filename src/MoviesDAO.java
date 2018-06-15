28cffd2f0a8e60bfed29ccbef976e2db8ef732e0
Aleksander Dytko <olek.dytko@gmail.com>
2018-06-15 14:55:35 +0200

    Dodane metody add, remove, get

--- src/MoviesDAO.java
+++ src/MoviesDAO.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class MoviesDAO {
 
@@ -22,6 +20,60 @@ public class MoviesDAO {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void addMovie(Movie movie){
+        final String sql = "insert into movies(title, director, URL, year) values(?, ?, ?,?)";
+        try {
+            PreparedStatement prepStmt = connection.prepareStatement(sql);
+            prepStmt.setString(1, movie.getTitle());
+            prepStmt.setString(2, movie.getDirector());
+            prepStmt.setString(3, movie.getURL());
+            prepStmt.setString(4, movie.getYear());
+            prepStmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Could not save record");
+            e.printStackTrace();
+        }
+
+    }
+
+    public Movie getMovie(int id){
+        final String sql = "select id, title, director, URL, year from movies where id = ?";
+        try {
+            PreparedStatement prepStmt = connection.prepareStatement(sql);
+            prepStmt.setInt(1, id);
+            ResultSet result = prepStmt.executeQuery();
+            if (result.next()) {
+                Movie movie = new Movie();
+                movie.setId(result.getInt("id"));
+                movie.setTitle(result.getString("title"));
+                movie.setDirector(result.getString("director"));
+                movie.setURL(result.getString("URL"));
+                movie.setYear(result.getString("year"));
+                return movie;
+            }
+        } catch (SQLException e) {
+            System.out.println("Could not get employee");
+        }
+        return null;
+    }
+
+    public void getAllMovies(){
+
+    }
+
+    public void removeMovie(int id){
+
+        final String sql = "delete from movies where id = ?";
+        try {
+            PreparedStatement prepStmt = connection.prepareStatement(sql);
+            prepStmt.setLong(1, id);
+            prepStmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Could not delete row");
+        }
+
     }
 
     public void close() {
